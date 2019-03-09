e284ed0b52bdee1e9adec4f453ea328973f1fdd2
AbaloneDoctor <spencerpao@gmail.com>
2019-03-09 15:38:12 -0800

    Committing VS files, testing if gitIgnore is working

---
+++ GitTest/GitTest/GitTest.cpp
@@ -0,0 +1,15 @@
+// GitTest.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "Hey, I'm in the cloud!" << endl;
+	
+    return 0;
+}
+
