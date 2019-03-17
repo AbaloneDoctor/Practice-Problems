8ed9badee656268b2604c80dcef1fc2505798020
AbaloneDoctor <spencerpao@gmail.com>
2019-03-16 20:33:45 -0700

    implemented backtrack method and a backtracking search

--- Letter Combinations of Phone Number/Letter Combinations of Phone Number/LetterCombinationsofPhoneNumber.cpp
+++ Letter Combinations of Phone Number/Letter Combinations of Phone Number/LetterCombinationsofPhoneNumber.cpp
@@ -1,6 +1,6 @@
 // LetterCombinationsofPhoneNumber.cpp : Defines the entry point for the console application.
 // Given a string containing digits from 2-9 inclusive, return all possible letter combinations that the number could represent.
-
+//March 15-16
 
 #include "stdafx.h"
 #include <vector>
@@ -26,9 +26,8 @@ vector<string> letterCombinationsBrute(string digits) {
 		combos.push_back(newString);
 	}
 	for (int itr = 1; itr < digits.size(); itr++) {							//for every number in string digits
-		int num = digits[itr] - '0';
 		vector<string> updatedCombos;
-
+		int num = digits[itr] - '0';
 		for (string combItr : combos) {										//for every word in current combos		
 			for (int currItr = 0; currItr < arr[num].size(); currItr++) {	//for every letter in new combo (so 3 or 4)
 				string newCombo;											//generate a new word combo 
@@ -42,7 +41,56 @@ vector<string> letterCombinationsBrute(string digits) {
 	return combos;
 }
 
-void printList( vector<string> list ) {
+//Solution using backtracking/recursion
+//timing is faster because it doesn't go through each path. When it reaches end of 1 combo, it steps back 1 letter and does the 
+//next letter of the group in the for(:) loop
+class solution {
+public:
+	vector<string> combos;
+	string t9[10] = { "", "", "abc", "def", "ghi", "jkl", "mno" , "pqrs", "tuv", "wxyz" };
+
+	void backtrack( string newCombo, string remainingDigits, string letter) {
+		if (remainingDigits.size() == 0) {
+			newCombo += letter;
+			combos.push_back(newCombo);
+		}
+		else {
+			newCombo += letter;
+			int currDigit = remainingDigits[0] - '0';
+			string currLetters = t9[currDigit];
+			remainingDigits.erase(remainingDigits.begin());		//erase first digit in remaining digits
+			for (char itr : currLetters) {						
+				string singleLetter(1, itr);					//properly convert from char to string
+				backtrack(newCombo, remainingDigits, singleLetter);
+			}
+		}
+	}
+
+	vector<string> letterCombinationsBacktrack(string digits) {
+		backtrack("", digits, "");
+		return combos;
+	}
+
+	//search using backtrack
+	bool search(string digits, string combo) {
+		if (combo.size() > digits.size()) return false;		//combo can be shorter, but not longer (ie combo has been input by digits)
+		if (combo.size() == 0) return true;
+		else {
+			string currLetters = t9[digits[0] - '0'];
+			for (char itr : currLetters) {
+				string itrString(1, itr);
+				if (combo[0] == itr) {
+					digits.erase(digits.begin());
+					combo.erase(combo.begin());
+					return search(digits, combo);			//will return true if they are all recursively true
+				}
+			}
+			return false;
+		}
+	}
+};
+
+void printList( vector<string> list ) {						//print function for both brute and backtrack methods
 	int count = 0;
 	for (string s : list) {
 		cout << s << " ";
@@ -52,16 +100,37 @@ void printList( vector<string> list ) {
 	cout << "Number of combinations: " << count << endl;
 }
 
-
-int main()
-{
+void letterCombinationsDEMO() {
 	string input;
 	while (1) {
+		solution sol;
 		cout << "input t9 number: ";
 		cin>>input;
 		if (input == "end") break;
-		printList( letterCombinationsBrute(input) );
+		//printList( letterCombinationsBrute(input) );
+		printList(sol.letterCombinationsBacktrack(input));	
+	}
+}
+
+void letterCombinationsSearchDEMO() {
+	string input;
+	string input2;
+	while (1) {
+		solution sol;
+		cout << "input digits and combo: " << endl;
+		cin >> input;
+		if (input == "end") break;
+		cin >> input2;
+		cout << sol.search(input, input2) << endl;
 	} 
+}
+
+int main()
+{
+
+	letterCombinationsDEMO();
+	letterCombinationsSearchDEMO();
+ 
 	cin.get();
 
     return 0;
