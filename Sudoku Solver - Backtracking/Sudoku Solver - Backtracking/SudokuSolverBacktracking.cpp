c0c3d8c5c21222157337702c9356d67c68948dbd
AbaloneDoctor <spencerpao@gmail.com>
2019-03-22 15:43:37 -0700

    Initial file, return to complete

---
+++ Sudoku Solver - Backtracking/Sudoku Solver - Backtracking/SudokuSolverBacktracking.cpp
@@ -0,0 +1,11 @@
+// SudokuSolverBacktracking.cpp : Defines the entry point for the console application.
+// Use backtracking to solve a given sudoku puzzle. Print invalid if puzzle doesn't follow sudoku rules
+
+#include "stdafx.h"
+
+
+int main()
+{
+    return 0;
+}
+
