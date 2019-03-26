6047560a3b1870d43e00046f9a8098b3b09d869c
AbaloneDoctor <spencerpao@gmail.com>
2019-03-25 18:31:37 -0700

    added comments for review understanding

--- Letter Combinations of Phone Number/Letter Combinations of Phone Number/LetterCombinationsofPhoneNumber.cpp
+++ Letter Combinations of Phone Number/Letter Combinations of Phone Number/LetterCombinationsofPhoneNumber.cpp
@@ -13,26 +13,25 @@ void printList(vector<string> arr);											//forward declaration for debuggin
 
 //Brute force method
 //Idea: go through
-//timing is O(3^N), N is length of string of digits
+//timing is O(N^3), N is length of string of digits
 vector<string> letterCombinationsBrute(string digits) {
 	string arr[10] = { "0", "0", "abc", "def", "ghi", "jkl", "mno" , "pqrs", "tuv", "wxyz" };
 	vector<string> combos;
 
-	//seed the first digit's letters in
-	int seedNum = digits[0] - '0';
-	for (char letter : arr[seedNum]) {
+	//seed the first digit's letters in										//EG. 234, seedNum = 2 ([2]34)
+	int seedNum = digits[0] - '0';											//arr[2] = "abc"
+	for (char letter : arr[seedNum]) {										//combos = { "a", "b", "c"}
 		string newString;
 		newString.push_back(letter);
 		combos.push_back(newString);
 	}
-	for (int itr = 1; itr < digits.size(); itr++) {							//for every number in string digits
+	for (int itr = 1; itr < digits.size(); itr++) {							//for every remaining number in string digits (3 and 4)
 		vector<string> updatedCombos;
 		int num = digits[itr] - '0';
-		for (string combItr : combos) {										//for every word in current combos		
-			for (int currItr = 0; currItr < arr[num].size(); currItr++) {	//for every letter in new combo (so 3 or 4)
-				string newCombo;											//generate a new word combo 
-				newCombo += combItr;
-				newCombo += arr[num][currItr];
+		for (string combItr : combos) {										//for every word in current combos	{"a", "b", "c"}	
+			for (int currItr = 0; currItr < arr[num].size(); currItr++) {	//for every letter in new combo { "d", "e", "f"}, { "g", "h", "i" }
+				string newCombo = combItr;									//generate a new word combo, start with prev combo { "a", "b", "c" } 
+				newCombo += arr[num][currItr];								//append current combo {"ad", "ae", "af" , "bd", "be" .. }
 				updatedCombos.push_back(newCombo);
 			}
 		}
@@ -49,21 +48,21 @@ public:
 	vector<string> combos;
 	string t9[10] = { "", "", "abc", "def", "ghi", "jkl", "mno" , "pqrs", "tuv", "wxyz" };
 
-	void backtrack( string newCombo, string remainingDigits, string letter) {
-		if (remainingDigits.size() == 0) {
+	void backtrack( string newCombo, string remainingDigits, string letter) {	//EG digits = 234
+		if (remainingDigits.size() == 0) {						//base case, when digits is shrunken to nothing
 			newCombo += letter;
 			combos.push_back(newCombo);
 		}
 		else {
-			newCombo += letter;
-			int currDigit = remainingDigits[0] - '0';
+			newCombo += letter;									// ""
+			int currDigit = remainingDigits[0] - '0';			// "abc"
 			string currLetters = t9[currDigit];
-			remainingDigits.erase(remainingDigits.begin());		//erase first digit in remaining digits
-			for (char itr : currLetters) {						
-				string singleLetter(1, itr);					//properly convert from char to string
+			remainingDigits.erase(remainingDigits.begin());		//erase first digit in remaining digits,	digits = []34
+			for (char itr : currLetters) {						// for each  'a', 'b', 'c'
+				string singleLetter(1, itr);					//properly convert from char to string, 1 is size
 				backtrack(newCombo, remainingDigits, singleLetter);
-			}
-		}
+			}													//recursively (backtrack) creates: { "adg", "adh", "adj", "aeg", "aeh", "aei"...}
+		}														//order: 111, 112, 113, 121, 122, 123, 131, 132, 133, 211, 212, 213, 221, 222...
 	}
 
 	vector<string> letterCombinationsBacktrack(string digits) {
@@ -74,14 +73,14 @@ public:
 	//search using backtrack
 	bool search(string digits, string combo) {
 		if (combo.size() > digits.size()) return false;		//combo can be shorter, but not longer (ie combo has been input by digits)
-		if (combo.size() == 0) return true;
+		if (combo.size() == 0) return true;					//base case: you've gone through every letter and nothing has returned false, return true
 		else {
-			string currLetters = t9[digits[0] - '0'];
-			for (char itr : currLetters) {
-				string itrString(1, itr);
-				if (combo[0] == itr) {
-					digits.erase(digits.begin());
-					combo.erase(combo.begin());
+			string currLetters = t9[digits[0] - '0'];		//return letter group of first num
+			for (char itr : currLetters) {					
+				string itrString(1, itr);					//convert itr letter(char) to a string of a single leter
+				if (combo[0] == itr) {						//if matches, keep searching, if not nothing happens, but will default to return false
+					digits.erase(digits.begin());			//	if none of the letters match	
+					combo.erase(combo.begin());				//erase first digit and first letter of combo since we've matched
 					return search(digits, combo);			//will return true if they are all recursively true
 				}
 			}
@@ -104,7 +103,7 @@ void letterCombinationsDEMO() {
 	string input;
 	while (1) {
 		solution sol;
-		cout << "input t9 number: ";
+		cout << "input t9 number to generate all possible t9 words: ";
 		cin>>input;
 		if (input == "end") break;
 		//printList( letterCombinationsBrute(input) );
@@ -117,7 +116,7 @@ void letterCombinationsSearchDEMO() {
 	string input2;
 	while (1) {
 		solution sol;
-		cout << "input digits and combo: " << endl;
+		cout << "Search: input digits and combo you want to check: " << endl;
 		cin >> input;
 		if (input == "end") break;
 		cin >> input2;
