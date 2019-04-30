661fed8e4004f6c8092a41bbc5038a724e60223e
AbaloneDoctor <spencerpao@gmail.com>
2019-04-29 23:02:14 -0700

    O(N) imp, also interesting iterator demo

---
+++ Reverse Strings (E)/Reverse Strings (E)/ReverseStringsE.cpp
@@ -0,0 +1,64 @@
+// ReverseStringsE.cpp : Defines the entry point for the console application.
+// https://leetcode.com/problems/reverse-string/
+//reverse string in place
+
+#include "stdafx.h"
+#include <iostream>
+#include <vector>
+#include <stack>
+
+using namespace std;
+
+//using 2 ptrs to switch
+//O(N) time complexity,  O(1) space complexity
+void reverseString(vector<char>& s) {
+	//2 ptrs, one at start one at end
+	//while first ptr is smaller than last ptr
+		//swap s[i++] with s[j--] 
+	int i = 0;
+	int j = s.size()-1;
+	while (i < j) {
+		swap(s[i++], s[j--]);
+	}
+}
+
+
+//using stack
+//doesn't use O(1) memory though
+void reverseStringStack(vector<char>& s) {
+	//for each letter in s, push into stack
+	//then go through and top/pop back into s
+	stack <char> stack;
+	for (auto itr = s.begin(); itr != s.end(); ) {
+		stack.push(*itr++);
+		//s.erase(itr);
+	}
+	//this will refer to actual position in s if you use address
+	for (char &itr : s) {
+		itr = stack.top();
+		stack.pop();
+	}
+	return;
+}
+
+
+//returning a vector of iterator range?
+string reverseStringVectorItr(string s) {
+	//return { s.end(), s.begin() };	//triggers "invalid iterator range"
+//	return { s.rbegin(), s.rend() };
+	//return string(s.rbegin(), s.rend());		//same solution without string constructor
+	return { s.begin(), s.begin()+2 };
+}
+
+int main()
+{
+	vector<char> word = { 'a', 'b', 'c', 'd', 'e', 'f', 'g' };
+	reverseString(word);
+	reverseStringStack(word);
+
+	string wordString(word.begin(), word.end());
+	wordString = reverseStringVectorItr(wordString);
+
+    return 0;
+}
+
