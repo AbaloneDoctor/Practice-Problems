7a631ba48c29d9d4a29457a0ca2ed5bc90bc0edc
AbaloneDoctor <spencerpao@gmail.com>
2019-05-04 21:02:35 -0700

    DP: tabulation

--- House Robber/House Robber/HouseRobber.cpp
+++ House Robber/House Robber/HouseRobber.cpp
@@ -9,6 +9,7 @@
 
 using namespace std;
 
+//helper function
 int robRecurse( vector<int> nums, int itr, int max, int currMax ) {
 	if (itr > nums.size() - 1) {
 		//currMax += nums[itr];
@@ -17,7 +18,9 @@ int robRecurse( vector<int> nums, int itr, int max, int currMax ) {
 	else {
 		currMax += nums[itr];
 		if (currMax > max) max = currMax;
+		//1 space in between
 		int res1 = robRecurse(nums, itr + 2, max, currMax);
+		//2 spaces in between
 		int res2 = robRecurse(nums, itr + 3, max, currMax);
 		return (res1 >= res2) ? res1 : res2;
 	}
@@ -35,12 +38,56 @@ int rob(vector<int>& nums) {
 	
 }
 
+//using dynamic programming
+//09:06 PM - 05.03.19 : 
+// https://leetcode.com/problems/house-robber/discuss/55838/DP-O(N)-time-O(1)-space-with-easy-to-understand-explanation
+//int robDP(vector<int>& nums) {
+//}
+
+/*
+CASES: 
+	-current n, rob or not to rob:
+		-rob n, then add to max of rob(n-2, n-3)
+		-don't rob n, add to max of rob (n-1, n-2)
+
+*/
+
+int robDP(vector<int>& nums) {
+
+	if (nums.empty()) {
+		return 0;
+	}
+
+	vector<int> arr;
+	unsigned int numsLength = nums.size();
+
+	if (numsLength == 1) {
+		return nums[0];
+	}
+	if (numsLength == 2) {
+		return max(nums[0], nums[1]);
+	}
+	else {
+		arr.push_back( nums[0]);
+		arr.push_back( nums[1]);
+		for (int i = 2; i < numsLength; i++) {
+			int current = nums[i];
+			//2 choices: take current + 1 before prev, or take prev and skip current
+			int prevMax = max(current + arr[i - 2], arr[i - 1]);
+			arr.push_back(max(prevMax, current));
+		}
+	}
+	return arr[numsLength - 1];
+
+}
+
 int main()
 {
 	vector<int> arr = { 1,3,1, 9 , 1 , 9 , 1 , 9 };
+	vector<int> arr3 = { 1,3,1, 9 , 1 , 50 , 19 , 9 };
 	vector<int> arr2 = {9, 9, 9, 9, 9};
-	int res = rob(arr2);
-	
+	int res = rob(arr3);
+	res = robDP(arr3);
     return 0;
 }
 
